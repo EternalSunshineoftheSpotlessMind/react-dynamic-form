2c437ae2d0e066b8454a2d6963ca38e6f74d029a
John Sarmiento <johnsarmiento117@gmail.com>
2024-03-27 20:22:26 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+//Bootstrap components
+import { Col, Row, Form, Button, Table, Accordion } from 'react-bootstrap';
+
+//Firebase imports
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { db } from './firebase';
 
 function App() {
+  const [inputs, setInputs] = useState([
+    {
+      unitNumberTag: '',
+      serviceAreaType: '',
+      serviceAreaSize: ''
+    }
+  ])
+
+  const handleChange = (index, event) => {
+    let data = [...inputs];
+    data[index][event.target.name] = event.target.value;
+    setInputs(data);
+  }
+
+  //For button to add new row
+  const addRow = () => {
+    let newRow = {
+      unitNumberTag: '',
+      serviceAreaType: '',
+      serviceAreaSize: ''
+    }
+
+    setInputs([...inputs, newRow])
+  }
+
+  //Submit function
+  const submit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+
+  //Add to firebase
+  // const addFloor = async (e) => {
+  //   e.preventDefault();  
+   
+  //   try {
+  //       const rowRef = await addDoc(collection(db, "floors"), {
+  //         todo: todo,    
+  //       });
+  //       console.log("Document written with ID: ", rowRef.id);
+  //     } catch (e) {
+  //       console.error("Error adding document: ", e);
+  //     }
+  // }
+
+  //Remove row function
+  const removeRow = (index) => {
+    let data = [...inputs];
+    data.splice(index, 1);
+    setInputs(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion defaultActiveKey={['0']} alwaysOpen>
+        <Accordion.Item eventKey="0">
+            <Accordion.Header>Description of the Saleable Area</Accordion.Header>
+            <Accordion.Body>
+              <Form>
+                <Form.Group>
+                  <Row>
+                    <Col></Col>
+                    <Col>Unit No./Tag</Col>
+                    <Col>Type</Col>
+                    <Col>Area (sqm)</Col>
+                    <Col></Col>
+                  </Row>
+                </Form.Group>
+                {inputs.map((input, index) => {
+                  return (
+                    <Form.Group key={index}>
+                      <Row>
+                        {/* Show index/row number */}
+                        <Col>{index}</Col>
+
+                        {/* Unit No. */}
+                        <Col>
+                          <Form.Control
+                          type="text"
+                          placeholder="Unit No./Tag"
+                          name="unitNumberTag"
+                          value={input.unitNumberTag}
+                          onChange={event => handleChange(index, event)}
+                          />
+                        </Col>
+
+                        {/* Type */}
+                        <Col>
+                          <Form.Select 
+                          aria-label="Default select example"
+                          name="serviceAreaType"
+                          value={input.serviceAreaType}
+                          onChange={event => handleChange(index, event)}
+                          >
+                          <option value="">Select service area type</option>
+                          <option value="Roadway/Common Area">Roadway/Common Area</option>
+                          <option value="Stairs">Stairs</option>
+                          <option value="Transformer Room">Transformer Room</option>
+                          <option value="Generator Room">Generator Room</option>
+                          <option value="Ramp">Ramp</option>
+                          <option value="Lobby/Hallway">Lobby/Hallway</option>
+                          <option value="Residential Lobby">Residential Lobby</option>
+                          <option value="Corridors">Corridors</option>
+                          <option value="Fire Stairs">Fire Stairs</option>
+                          </Form.Select>
+                        </Col>
+
+                        {/* Area size */}
+                        <Col>
+                          <Form.Control 
+                          type="number"
+                          step="0.01"
+                          placeholder="Area (sqm)" 
+                          name="serviceAreaSize"
+                          value={input.serviceAreaSize}
+                          onChange={event => handleChange(index, event)}
+                          />
+                        </Col>
+
+                        {/* Remove row button */}
+                        <Col>
+                          <Button variant='primary' onClick={() => removeRow(index)}>
+                            Remove
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Form.Group>
+                  )
+                })}
+                <Button variant="primary" onClick={addRow}>
+                Add row
+                </Button>
+              </Form>
+            </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+      <Button variant="primary" onClick={submit}>
+      Submit
+      </Button>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB4pY4IF3UX2wq4Lgm9gYA9Ti5fswPZF5w",
+  authDomain: "capstone2236.firebaseapp.com",
+  projectId: "capstone2236",
+  storageBucket: "capstone2236.appspot.com",
+  messagingSenderId: "107227379508",
+  appId: "1:107227379508:web:ecab5e4cee06a01f555761",
+  measurementId: "G-DL1J9WSQS0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
